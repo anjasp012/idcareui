c1efe4388743a868a7de2f8e234c6c6fced6f17c
Anjasputra012 <anjasputra012@gmail.com>
2024-01-22 10:28:57 +0700

    fix back

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -1,15 +1,16 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="{{ route('admin.dashboard') }}">Stisla</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="{{ route('admin.dashboard') }}">St</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li>
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="{{ route('admin.dashboard') }}" class="nav-link"><i
+                        class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Pages</li>
             <li class=""><a class="nav-link" href="{{ route('admin.pages.sekilas-idcare-ui.index') }}"><i
