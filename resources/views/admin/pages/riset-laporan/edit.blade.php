3e7a8d1053a98d5060ea9b9f605085b8e38cc2fe
Anjasputra012 <anjasputra012@gmail.com>
2024-01-22 10:24:19 +0700

    riset

--- resources/views/admin/pages/riset-laporan/edit.blade.php
+++ resources/views/admin/pages/riset-laporan/edit.blade.php
@@ -11,9 +11,10 @@
         <section class="section">
             <div class="section-header">
                 <div class="section-header-back">
-                    <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                    <a href="{{ route('admin.pages.riset-laporan.index') }}" class="btn btn-icon"><i
+                            class="fas fa-arrow-left"></i></a>
                 </div>
-                <h1>Create New Riset</h1>
+                <h1>Edit Riset</h1>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Posts</a></div>
