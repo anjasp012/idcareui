1b3ae4edcda71025312ad8510e5e15af042b3805
Anjasputra012 <anjasputra012@gmail.com>
2024-01-22 10:23:27 +0700

    riset

--- resources/views/admin/pages/riset-laporan/edit.blade.php
+++ resources/views/admin/pages/riset-laporan/edit.blade.php
@@ -35,7 +35,8 @@
                                     <div class="form-group row mb-4">
                                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Penulis</label>
                                         <div class="col-sm-12 col-md-7">
-                                            <input type="text" name="penulis" class="form-control">
+                                            <input type="text" name="penulis" class="form-control"
+                                                value="{{ $data->penulis }}">
                                             @error('penulis')
                                                 <small class="text-danger">{{ $message }}</small>
                                             @enderror
