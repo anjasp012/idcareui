50594db8b1e4889c6ab623bbce6a63a9814d05e4
Anjasputra012 <anjasputra012@gmail.com>
2024-01-22 07:28:06 +0700

    kelas

--- resources/views/admin/pages/kelas/edit.blade.php
+++ resources/views/admin/pages/kelas/edit.blade.php
@@ -59,14 +59,19 @@
             filebrowserImageBrowseUrl: '/filemanager?type=Images',
             filebrowserImageUploadUrl: '/filemanager/upload?type=Images&_token=',
             filebrowserBrowseUrl: '/filemanager?type=Files',
-            filebrowserUploadUrl: '/filemanager/upload?type=Files&_token=' + CSRFToken
+            filebrowserUploadUrl: '/filemanager/upload?type=Files&_token=' + CSRFToken,
+            extraPlugins: 'justify,preview,simplebutton',
+            // skin: 'office2013',
         };
+        // CKEDITOR.config.contentsCss = ['/stisla/dist/assets/modules/ckeditor/skins/office2013/editor.css'];
         CKEDITOR.plugins.addExternal('justify', '/stisla/dist/assets/modules/ckeditor/plugins/justify/',
             'plugin.js');
+        CKEDITOR.plugins.addExternal('preview', '/stisla/dist/assets/modules/ckeditor/plugins/preview/',
+            'plugin.js');
+        CKEDITOR.plugins.addExternal('simplebutton',
+            '/stisla/dist/assets/modules/ckeditor/plugins/simplebutton/',
+            'plugin.js');
 
-        CKEDITOR.replace('body', {
-            CKEditorOptions,
-            extraPlugins: 'justify',
-        });
+        CKEDITOR.replace('body', CKEditorOptions);
     </script>
 @endpush
